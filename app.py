70abcda331179b543c6cd4b9150f998d7eddf953
Justin Shelton <justin@underlogic.io>
2017-10-26 14:53:54 -0500

    Fix redundant use of params in stream building

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_twitch_stream(user):
     return json.loads(r.text)
 
 
-def build_streamer_json(user, stream, participant_id, user_info):
+def build_streamer_json(stream, user_info):
     """Compile useful streamer information from a stream object.
 
     :param user: The username of the streamer
@@ -57,11 +57,14 @@ def build_streamer_json(user, stream, participant_id, user_info):
     :param participant_id: The user's Extra Life participant ID
     :return: A subset object of relevant streamer info
     """
+    participant_id = user_info.get('EXTRALIFE')
+    user = user_info.get('TWITCH')
     donate_url = 'http://www.extra-life.org/index.cfm?fuseaction=donorDrive.' \
                  'participant&participantID={}'.format(participant_id)
+    print(user_info)
     s = {
         'dispname': user_info['NAME'],
-        'username': user,
+        'username': user_info['TWITCH'],
         'playing': 'Offline',
         'viewers': 0,
         'url': 'https://www.twitch.tv/{}'.format(user),
@@ -73,7 +76,10 @@ def build_streamer_json(user, stream, participant_id, user_info):
     }
 
     if user_info.get('PUBG'):
-        s['pubg'] = pubg.get_stats_simple(user_info['PUBG'])
+        try:
+            s['pubg'] = pubg.get_stats_simple(user_info['PUBG'])
+        except KeyError as exc:
+            s['pubg'] = {}
 
     if not stream['stream']:
         return s
@@ -102,7 +108,7 @@ def get_streams(streamers):
         extralife = user.get('EXTRALIFE')
         try:
             stream = get_twitch_stream(twitch)
-            info = build_streamer_json(twitch, stream, extralife, user)
+            info = build_streamer_json(stream, user)
             streams.append(info)
         except requests.exceptions.HTTPError as htp:
             app.logger.error("Couldn't load user '%s': %s", twitch, htp)
